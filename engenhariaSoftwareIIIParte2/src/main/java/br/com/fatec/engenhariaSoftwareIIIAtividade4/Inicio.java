4cd8206a995581b9d03a8a0adf0b40004d4c86c1
mihoj <mihojp1@hotmail.com>
2020-10-12 15:50:45 -0300

    json funcionando

--- engenhariaSoftwareIIIParte2/src/main/java/br/com/fatec/engenhariaSoftwareIIIAtividade4/Inicio.java
+++ engenhariaSoftwareIIIParte2/src/main/java/br/com/fatec/engenhariaSoftwareIIIAtividade4/Inicio.java
@@ -1,13 +1,17 @@
 package br.com.fatec.engenhariaSoftwareIIIAtividade4;
 
+import java.io.IOException;
+
 import br.com.fatec.engenhariaSoftwareIIIAtividade4.pagamento.ArquivoJson;
 import br.com.fatec.engenhariaSoftwareIIIAtividade4.pagamento.Leitor;
 
 public class Inicio {
 
-	public static void main(String[] args) {
-		Leitor json = new ArquivoJson("Teste de criação");
-		System.out.println(json.escreverArquivo());
+	public static void main(String[] args) throws IOException {
+		
+		ArquivoJson json = new ArquivoJson();
+		
+		json.readFile();
 	}
 
 }
--- engenhariaSoftwareIIIParte2/src/main/java/br/com/fatec/engenhariaSoftwareIIIAtividade4/pagamento/ArquivoJson.java
+++ engenhariaSoftwareIIIParte2/src/main/java/br/com/fatec/engenhariaSoftwareIIIAtividade4/pagamento/ArquivoJson.java
@@ -1,33 +1,31 @@
 package br.com.fatec.engenhariaSoftwareIIIAtividade4.pagamento;
 
+import java.io.FileReader;
 import java.io.IOException;
+import java.io.Reader;
 import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
+import java.lang.reflect.Type;
+import com.google.gson.reflect.TypeToken;
 
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.google.gson.Gson;
 
-public class ArquivoJson extends Leitor{
-	String arquivoJson;
-
-	
-	public ArquivoJson(String arquivoJson) {
-		this.arquivoJson = arquivoJson;
-	}
+public class ArquivoJson{
 
 
-	@Override
-	protected String construirArquivo(){
-		Gson gson = new Gson(); 
-		try {
-			//ObjectMapper mapper = new ObjectMapper();
-			//String arquivo = readFile("c:\\remessa.json", StandardCharsets.UTF_8);
-			//List<Pagamento> participantJsonList = mapper.readValue(arquivo, new TypeReference<List<Pagamento>>(){});
-//			String arquivo = readFile("c:\\remessa.json", StandardCharsets.UTF_8);
+
+	//protected String construirArquivo(){
+//		Gson gson = new Gson(); 
+//		try {
+//			//ObjectMapper mapper = new ObjectMapper();
+//			//String arquivo = readFile("c:\\remessa.json", StandardCharsets.UTF_8);
+//			//List<Pagamento> participantJsonList = mapper.readValue(arquivo, new TypeReference<List<Pagamento>>(){});
+//			String arquivo = readFile("C:\\Users\\mihoj\\OneDrive\\Documentos\\Faculdade\\ES3\\arquivos IV\\remessa.json", StandardCharsets.UTF_8);
 //			arquivo = arquivo.replace("[", "");
 //			arquivo = arquivo.replace("]", "");
 //			String [] pagamentoUnitarios = arquivo.split("},");
@@ -40,17 +38,27 @@ public class ArquivoJson extends Leitor{
 //				System.out.println(pagamento);
 //			}
 //			return arquivo;
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
+//		} catch (IOException e) {
+//			e.printStackTrace();
+//		}
 		
 
-		return null;
-	}
+	//	return null;
+	//}
 	
-	static String readFile(String path, Charset encoding) throws IOException{
-		byte[] encoded = Files.readAllBytes(Paths.get(path));
-		return new String(encoded, encoding);
+	public void readFile() throws IOException{
+		Gson gson = new Gson(); 
+		
+		Reader reader = new FileReader("C:\\Users\\mihoj\\OneDrive\\Documentos\\Faculdade\\ES3\\arquivos IV\\remessa.json");
+		Type listType = new TypeToken<List<Pagamento>>() {}.getType();
+		ArrayList<Pagamento> pagamentos = gson.fromJson(reader, listType);
+		int i = 1;
+		for (Pagamento pagamento : pagamentos) {
+			System.out.println(pagamento.getNome());
+			System.out.println(i +" - "+ pagamento.getParcelas());
+			i++;
+		}
+
 	}
 	
 }
