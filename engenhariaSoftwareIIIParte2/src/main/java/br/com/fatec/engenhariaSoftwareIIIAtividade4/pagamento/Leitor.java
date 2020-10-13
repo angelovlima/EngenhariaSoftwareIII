fc6568f8ca294bb03d2ad48473512e04cb8c89c2
mihoj <mihojp1@hotmail.com>
2020-10-13 00:45:00 -0300

    fix leitor

--- engenhariaSoftwareIIIParte2/src/main/java/br/com/fatec/engenhariaSoftwareIIIAtividade4/pagamento/Leitor.java
+++ engenhariaSoftwareIIIParte2/src/main/java/br/com/fatec/engenhariaSoftwareIIIAtividade4/pagamento/Leitor.java
@@ -11,9 +11,17 @@ public class Leitor {
 		
 		for (Pagamento pagamento : pagamentos) {
 			if(ValidadorCpf.valida(pagamento.getCpf())) {
-				System.out.println(pagamento.getNome() != null ? pagamento.getNome() : null);
-				System.out.println(pagamento.getParcelas() != null ? pagamento.getParcelas() : null);
-				System.out.println(pagamento.getCpf() != null ? pagamento.getCpf() : null);
+				System.out.println("\n++++++++++++++++++NOVA REMESSA++++++++++++++++\n");
+				System.out.println(pagamento.getNome() != null ? pagamento.getNome() : "Não contém");
+				System.out.println(pagamento.getParcelas() != null ? pagamento.getParcelas() : "Não contém");
+				System.out.println(pagamento.getCpf() != null ? pagamento.getCpf() : "Não contém");
+				System.out.println(pagamento.getData()!= null ? pagamento.getData() : "Não contém");
+				System.out.println(pagamento.getNomeTitular()!= null ? pagamento.getNomeTitular() : "Não contém");
+				System.out.println(pagamento.getNumeroBoleto()!= null ? pagamento.getNumeroBoleto() : "Não contém");
+				System.out.println(pagamento.getNumeroCartao()!= null ? pagamento.getNumeroCartao() : "Não contém");
+				System.out.println(pagamento.getValor()!= null ? pagamento.getValor() : "Não contém");
+				System.out.println(pagamento.getBancoPagamento()!= null ? "Banco de Pagamento: "+pagamento.getBancoPagamento() : "Não contém");
+				System.out.println(pagamento.getBancoRecebimento()!= null ? "Banco de Recebimento: "+pagamento.getBancoRecebimento() : "Não contém");
 			}
 		}
 	}
